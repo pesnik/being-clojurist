26fd1ddb5b1ce25ad6f303e747ce750a22f9c06b
Md. Rakibul Hasan <rakibul.hasan@trucklagbe.com>
2023-07-10 18:08:38 +0600

    learnt something about clojures functional composition

--- braveclojure.com/chapter5.clj
+++ braveclojure.com/chapter5.clj
@@ -8,4 +8,23 @@
   [text]
   (s/replace (s/trim text) #"lol" "LOL"))
 
-(clean "What a lame joke! lol")
+(def input "What a lame joke! lol")
+(def new-text (clean input))
+(println input)
+(println new-text)
+
+;; Functional composition
+((comp inc *) 2 3)
+
+(def students
+  {:name "Rakibul Hasan"
+   :scores {:math 90
+            :english 89
+            :religion 64}})
+(def math-score (comp :math :scores))
+(def english-score (comp :english :scores))
+(def religion-score (comp :religion :scores))
+
+(math-score students)
+(english-score students)
+(religion-score students)
