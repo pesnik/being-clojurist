5f81cd97407502e645d8982c0014d03a37c2b480
Md. Rakibul Hasan <rakibul.hasan@trucklagbe.com>
2023-07-11 12:48:31 +0600

    finished reading chapter6
    - now the time to work with the chapter6 project for building the lamda
    xml draw

--- braveclojure.com/chapter6.clj
+++ braveclojure.com/chapter6.clj
@@ -2,9 +2,11 @@
 (def hello "Hello World!")
 (println hello)
 
-(clojure.core/refer 'chapter6-test :only ['expr])
-(println (expr 6))
+(clojure.core/refer 'chapter6-test :rename {'expr 'addit})
+(clojure.core/alias 'aliases 'chapter6-test-alias)
+(println (addit 6))
 (println (expr2 6))
+(println aliases/yahoo)
 
 (ns chapter6-test)
 (clojure.core/refer 'chapter6)
@@ -14,3 +16,13 @@
 (def expr2 (fn [arg] (str "You passed " arg)))
 
 (expr2 45)
+
+(ns chapter6-test-alias)
+(def yahoo "@yahoo.com")
+
+;; above all the methods to work with the ns are for working with REPL.
+;; for a project file we'll require things like the following
+(ns chapter6-actual
+  (:require [chapter6-test-alias :as aliases]))
+
+(println yahoo)
